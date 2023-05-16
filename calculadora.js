445d6812bb14504ab506358f72ba2549911c0b97
JuanGomez19 <jgjuangomez90@gmail.com>
2023-05-16 20:01:52 -0300

    mi primer commit

---
+++ calculadora.js
@@ -0,0 +1,24 @@
+// Funcion Expresada
+
+const sumar = function (a, b) {
+    return a + b
+}
+
+const restar = function(a,b){
+    return a -b 
+}
+
+const multiplicar = function (a,b) {
+    return a * b
+}
+
+const dividir = function (a,b) {
+    return a / b   
+}
+
+module.exports = {
+    sumar, 
+    restar,
+    multiplicar,
+    dividir
+}
